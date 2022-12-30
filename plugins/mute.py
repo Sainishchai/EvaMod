d20eaef9ca566e10dd92171f5ba30f276b685d4b
Third-Sense <108891477+Third-Sense@users.noreply.github.com>
2022-12-30 18:08:16 +0530

    Update mute.py

--- plugins/mute.py
+++ plugins/mute.py
@@ -6,8 +6,8 @@ from pyrogram.types import (
     ChatPermissions
 )
 from plugins.admin_check import admin_check
-from plugins.josprojects.help_func.extract_user import extract_user
-from plugins.josprojects.help_func.string_handling import extract_time
+from plugins.extract_user import extract_user
+from plugins.string_handling import extract_time
 
 
 @Client.on_message(filters.command("mute"))
