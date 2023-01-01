49311865f1b5781649ea7bb01a1a3b35cefdbebc
Third-Sense <108891477+Third-Sense@users.noreply.github.com>
2023-01-01 22:19:40 +0530

    Update unban.py

--- plugins/unban.py
+++ plugins/unban.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
-from plugins.admin_check import admin_check
-from plugins.extract_user import extract_user
+from plugins.helper_functions.admin_check import admin_check
+from plugins.helper_functions.extract_user import extract_user
 
 
 @Client.on_message(filters.command(["unban", "unmute"]))
