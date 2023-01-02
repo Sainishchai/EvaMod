7162ba3246500aa800e0f5cf98a03e7ef336770a
LᴀL <89661349+Devikaiii@users.noreply.github.com>
2023-01-02 08:43:13 +0530

    Update ban.py

--- plugins/ban.py
+++ plugins/ban.py
@@ -1,8 +1,8 @@
 
 from pyrogram import Client, filters
-from plugins.admin_check import admin_check
-from plugins.extract_user import extract_user
-from plugins.string_handling import extract_time
+from plugins.helper_functions.admin_check import admin_check
+from plugins.helper_functions.extract_user import extract_user
+from plugins.helper_functions.string_handling import extract_time
 
 
 @Client.on_message(filters.command("ban"))
