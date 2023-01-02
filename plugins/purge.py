d1e2f456fe223fa58f80fe138e02917913800a70
LᴀL <89661349+Devikaiii@users.noreply.github.com>
2023-01-02 08:40:49 +0530

    Update purge.py

--- plugins/purge.py
+++ plugins/purge.py
@@ -4,8 +4,8 @@ Syntax: .purge"""
 import asyncio
 from pyrogram import Client, filters
 from info import COMMAND_HAND_LER, TG_MAX_SELECT_LEN
-from plugins.admin_check import admin_check
-from plugins.cust_p_filters import f_onw_fliter
+from plugins.helper_functions.admin_check import admin_check
+from plugins.helper_functions.cust_p_filters import f_onw_fliter
 
 
 @Client.on_message(
